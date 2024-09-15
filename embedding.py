0da9e6ceb94a1f65484df2978123f6d0e0334b03
Devansh Bhardwaj <93275050+db2003@users.noreply.github.com>
2024-09-15 16:11:13 +0530

    Update embedding.py
    updated to support dockerisation

--- embedding.py
+++ embedding.py
@@ -90,7 +90,7 @@ index = pc.Index(index_name)
 
 
 # Initialize Redis client globally
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 def generate_cache_key(query, topk, threshold):
     # Generate a unique cache key based on query, topk, and threshold
